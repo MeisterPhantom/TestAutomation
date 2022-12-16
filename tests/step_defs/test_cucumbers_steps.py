6f4c7393e210c07fb2f5f801b31eeac1354f0402
MeisterPhantom <sinmentes@gmail.com>
2022-12-15 19:40:09 -0500

    parametrizando steps values

---
+++ tests/step_defs/test_cucumbers_steps.py
@@ -0,0 +1,23 @@
+from pytest_bdd import scenarios, given, when, then, parsers
+from modules.cucumbers import CucumberBasket
+
+
+@scenarios('../features/cucumbers.feature')
+@given(parsers.cfparse('the basket has "{initial:Number}" cucumbers', extra_types=dict(Number=int)), target_fixture='basket')
+def basket(initial):
+    return CucumberBasket(initial_count=initial)
+
+
+@when(parsers.cfparse('"{some:Number}" cucumbers are added to the basket', extra_types=dict(Number=int)))
+def add_cucumbers(basket, some):
+    basket.add(some)
+
+
+@when(parsers.cfparse('"{some:Number} cucumbers are removed from the basket"', extra_types=dict(Number=int)))
+def remove_cucumbers(basket, some):
+    basket.remove(some)
+
+
+@then(parsers.cfparse('the basket contains "{total:Number}" cucumbers', extra_types=dict(Number=int)))
+def basket_has_total(basket, total):
+    assert basket.count == total
